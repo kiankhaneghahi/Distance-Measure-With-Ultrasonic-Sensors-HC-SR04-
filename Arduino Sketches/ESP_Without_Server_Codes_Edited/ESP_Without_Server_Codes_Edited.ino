185743bad52f76f43780c32000a8c7686cf37787
kiankhaneghahi <42879334+kiankhaneghahi@users.noreply.github.com>
2020-09-07 20:39:52 +0430

    Add files via upload

---
+++ Arduino Sketches/ESP_Without_Server_Codes_Edited/ESP_Without_Server_Codes_Edited.ino
@@ -0,0 +1,25 @@
+const int TriggerPin = 4;
+const int EchoPin = 5;
+long Duration;
+int DistanceCm;
+const float SoundSpeed = 348;
+
+void setup() {
+Serial.begin(9600); 
+pinMode(TriggerPin, OUTPUT);
+pinMode(EchoPin, INPUT);
+}
+
+void loop() {
+digitalWrite(TriggerPin, LOW);
+delayMicroseconds(2);
+digitalWrite(TriggerPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(TriggerPin, LOW);
+Duration = pulseIn(EchoPin, HIGH);
+DistanceCm= (Duration*100*0.000001*SoundSpeed)/2; 
+Serial.print("Distance: "); 
+Serial.print(DistanceCm); 
+Serial.print(" cm\n");
+delay(1000);
+}
---
+++ Arduino Sketches/SR04_Micro_Codes_Edited/SR04_Micro_Codes_Edited.ino
@@ -0,0 +1,84 @@
+
+unsigned long Time=0;
+const byte interruptPin1 = 2;
+const byte interruptPin2 = 3;
+const byte ThresholdPin = 7;
+const byte HVoffPin = 8;
+const byte Tx1Pin = 5;
+const byte Tx2Pin = 6;
+const byte EchoPin = 4;
+volatile byte PulseTrain = LOW;
+volatile byte state2 = LOW;
+volatile int Threshold = HIGH;
+volatile byte HVoff = HIGH;
+volatile byte Echo = LOW;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(interruptPin1, INPUT_PULLUP);
+  pinMode(interruptPin2, INPUT_PULLUP);
+  pinMode(ThresholdPin, OUTPUT);
+  pinMode(HVoffPin, OUTPUT);
+  pinMode(Tx1Pin, OUTPUT);
+  pinMode(Tx2Pin, OUTPUT);
+  pinMode(EchoPin, OUTPUT);
+  digitalWrite(ThresholdPin, Threshold);
+  digitalWrite(HVoffPin, HVoff);
+  digitalWrite(Tx1Pin, LOW);
+  digitalWrite(Tx2Pin, LOW);
+  digitalWrite(EchoPin, Echo);
+  attachInterrupt(digitalPinToInterrupt(interruptPin1), Trigger, RISING);
+  attachInterrupt(digitalPinToInterrupt(interruptPin2), Signal, RISING);
+}
+
+void loop() {
+  while(PulseTrain = HIGH){
+    for(int i = 1; i <= 8; i++){
+      digitalWrite(Tx1Pin, HIGH);
+      digitalWrite(Tx2Pin, LOW);
+      delayMicroseconds(12);
+      digitalWrite(Tx1Pin, LOW);
+      digitalWrite(Tx2Pin, HIGH);
+      delayMicroseconds(12);
+      
+    }
+    digitalWrite(Tx2Pin, LOW);
+    Echo = HIGH;
+    digitalWrite(EchoPin, Echo);
+    Threshold = HIGH;
+    HVoff = HIGH;
+    digitalWrite(ThresholdPin,Threshold);
+    digitalWrite(HVoffPin, HVoff);
+    PulseTrain = LOW;
+    break;
+  }
+  while(Echo == HIGH){
+    if(Time>200){
+      digitalWrite(EchoPin, LOW);
+      Time = 0;
+      Serial.print("Long Echo Error");
+      Echo = LOW;
+      break;
+    }
+    else{
+      delay(1);
+      Time++;
+    }
+  }
+}
+
+void Trigger() {
+  delayMicroseconds(12);
+  Threshold = LOW;
+  digitalWrite(ThresholdPin,Threshold);
+  HVoff=LOW;
+  digitalWrite(HVoffPin, HVoff);
+  delayMicroseconds(248);
+  PulseTrain = HIGH;
+}
+
+void Signal() {
+  Echo = LOW;
+  digitalWrite(EchoPin, Echo);
+}
+
